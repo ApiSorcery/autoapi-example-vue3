4e697c0e288ce365f427c72e1c19dfbca2f9505a
ReganHe <heoo547627@126.com>
2025-07-01 16:24:10 +0800

    更换示例页面

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import Castor3UI from '@neweratech/castor3-ui'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(Castor3UI)
+app.mount('#app')
