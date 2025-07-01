ac2754acba488f627d83a291bb32e32dfacdffe8
ReganHe <heoo547627@126.com>
2025-07-01 14:55:20 +0800

    环境变量&服务地址配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,36 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
+  return {
+    base: env.VITE_GLOB_PUBLIC_PATH,
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+    server: {
+      host: true,
+      port: Number(env.VITE_PORT),
+      proxy: {
+        [env.VITE_GLOB_BASE_API]: {
+          target: 'http://150.158.52.214:9015/', // Prod
+          // target: 'http://localhost:9002/', // QA
+          // target: 'http://localhost:9001/', // Local
+          changeOrigin: true,
+          rewrite: (path) => path.replace(new RegExp(`^${env.VITE_GLOB_BASE_API}`), '')
+        }
+      }
+    },
+  }
 })
