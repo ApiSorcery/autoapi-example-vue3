28aa38870bef07b21273279b2365f5cc10bc1c04
ReganHe <heoo547627@126.com>
2026-01-30 17:24:29 +0800

    集成httpClient测试

--- src/apis/auto/demo/base.ts
+++ src/apis/auto/demo/base.ts
@@ -1,5 +1,5 @@
 import type { AxiosRequestConfig } from 'axios';
-import type { BlobResp } from './model';
+import type { BlobResp } from './httpClient/types/axios';
 import jsonRequest from './httpClient/requests/json';
 import blobRequest from './httpClient/requests/blob';
 
